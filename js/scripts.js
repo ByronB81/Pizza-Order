c44519ab097660003508a3dc03653debf8e5561f
byron blocker <byron blocker>
2017-03-31 11:40:44 -0700

    Spaghetti in my head

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 //Back End
 
-function Pizza(topping, size) {
-  this.topping = topping;
+function Pizza(name, size, topping, price) {
+  this.name= name;
   this.size = size;
+  this.topping = topping;
+  this.price = 0;
 }
-
-var price = 0
 
 Pizza.prototype.total = function() {
     if (this.size === "Small") {
@@ -18,7 +18,7 @@ Pizza.prototype.total = function() {
       console.log(this.topping.length);
     price = price + 1;
   }
-  var tax = (price * 0.18);
+  var tax = (price * 0.18).toFixed(2);
   total = price + tax;
   console.log(total);
 };
@@ -27,10 +27,45 @@ Pizza.prototype.total = function() {
 
 
 //Front End
-//
-// };
-// $(document).ready(function() {
-//
-//   $("#formOrder").submit(function(event) {
-//
-//     event.preventDefault();
+
+$(function() {
+  $("#blanks form").submit(function(event){
+    event.preventDefault();
+    var orderName = $("input#customer-name").val();
+    var pizzaSize = $("#pizza-size").val();
+    var pizzaTopping = $("input:checkbox[name-topping]:checked");
+    var newPizza= new Pizza(orderName, pizzaSize);
+    if (orderName){
+      if (pizzaSize) {
+        if (pizzaTopping) {
+
+          $.each($(pizzaTopping), function() {
+            newPizza.topping.push($(this).val());
+          })
+
+          newPizza.total();
+          console.log(total)
+          $("order-name").text(newPizza.name);
+          $(".pizza-size-output").text(newPizza.size);
+          $(".pizza-toppings-output1").text("WHY YOU NO WORK?");
+          alert("topping worked");
+
+        }
+        alert("sizer worked");
+
+        $(".pizza-price-output").text(newPizza.price);
+        $("#output").show();
+        $("#size-alert").hide();
+        $("#name-alert").hide();
+        resetFields();
+      } else{
+      $("#size-alert").show();
+      $("#name-alert").hide();
+    }
+    else{
+    $("#name-alert").show();
+    $("#size-alert").hide();
+    }
+
+  }
+});
